2e481395709425d8ad67823e10043e3528aa7bc4
gmurad97 <murad.dev@bk.ru>
2023-04-01 03:57:24 +0400

    add randomize color for letter

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
+function getRandomNumber(min, max) {
+    return Math.round(min - 0.5 + Math.random() * (max - min + 1))
+}
+
+function rstColor() {
+    let domSpanTags = document.getElementsByTagName("span");
+    for (let domSpanTag of domSpanTags) {
+        domSpanTag.style.color = "rgb(0, 0, 0)";
+    }
+}
+
 /*BEGIN - VARIANT 1*/
 
 let arrSpan = document.getElementsByClassName("variant")[0].getElementsByTagName("span");
 for (let spn of arrSpan) {
     spn.onclick = () => {
-        if(window.getComputedStyle(spn).color === "rgb(0, 0, 0)"){
-            spn.style.color = "rgb(170, 0, 0)";
+        if (window.getComputedStyle(spn).color === "rgb(0, 0, 0)") {
+            spn.style.color = "rgb(154, 59, 177)";
         }
-        else{
+        else {
             spn.style.color = "rgb(0, 0, 0)";
         }
     }
@@ -15,25 +26,48 @@ for (let spn of arrSpan) {
 /*ENDED - VARIANT 1*/
 
 /*BEGIN - VARIANT 2*/
-let variantContainer = document.getElementsByClassName("variant")[1];
-let spnInnerText = variantContainer.getElementsByTagName("span")[0].innerText;
-variantContainer.getElementsByTagName("span")[0].remove();
+let variantContainer2 = document.getElementsByClassName("variant")[1];
+let spnInnerText2 = variantContainer2.getElementsByTagName("span")[0].innerText;
+variantContainer2.getElementsByTagName("span")[0].remove();
 
-for(let x = 0; x <= spnInnerText.length - 1; x++){
+for (let x = 0; x <= spnInnerText2.length - 1; x++) {
     let domTag = document.createElement("span");
-    domTag.innerText = spnInnerText[x];
-    variantContainer.appendChild(domTag);
+    domTag.innerText = spnInnerText2[x];
+    variantContainer2.appendChild(domTag);
 }
 
-for(let spn of variantContainer.getElementsByTagName("span")){
+for (let spn of variantContainer2.getElementsByTagName("span")) {
     spn.onclick = () => {
-        if(window.getComputedStyle(spn).color === "rgb(0, 0, 0)"){
-            spn.style.color = "rgb(170, 0, 0)";
+        if (window.getComputedStyle(spn).color === "rgb(0, 0, 0)") {
+            spn.style.color = "rgb(0, 72, 186)";
         }
-        else{
+        else {
             spn.style.color = "rgb(0, 0, 0)";
         }
     }
 }
 
 /*ENDED - VARIANT 2*/
+
+/*BEGIN - VARIANT 3*/
+let variantContainer3 = document.getElementsByClassName("variant")[2];
+let spnInnerText3 = variantContainer3.getElementsByTagName("span")[0].innerText;
+variantContainer3.getElementsByTagName("span")[0].remove();
+
+for (let x = 0; x <= spnInnerText3.length - 1; x++) {
+    let domTag = document.createElement("span");
+    domTag.innerText = spnInnerText3[x];
+    variantContainer3.appendChild(domTag);
+}
+
+for (let spn of variantContainer3.getElementsByTagName("span")) {
+    spn.onclick = () => {
+        if (window.getComputedStyle(spn).color === "rgb(0, 0, 0)") {
+            spn.style.color = `rgb(${getRandomNumber(0, 255)}, ${getRandomNumber(0, 255)}, ${getRandomNumber(0, 255)})`;
+        }
+        else {
+            spn.style.color = "rgb(0, 0, 0)";
+        }
+    }
+}
+/*ENDED - VARIANT 3*/
